5babc3e039e84acaa25a0ddf946a1c92af69bd77
Junior Indart <juniorindart@gmail.com>
2018-04-04 12:13:42 +0900

    Update booststrap

--- dbsige/urls.py
+++ dbsige/urls.py
@@ -16,6 +16,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', 'dbsige.core.views.home' ),
+    url(r'^$', 'dbsige.core.views.home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
